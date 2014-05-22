02f6f3323cc46ac7eccf382cac04ede593485793
Malav Desai <malav1410@gmail.com>
2014-05-22 23:11:40 +0530

    commenting in code for find_by and has_many methods

--- mission_5/attacks/Malav/active_record/Base.rb
+++ mission_5/attacks/Malav/active_record/Base.rb
@@ -33,7 +33,7 @@ module ActiveRecord
         end
         i += 1
       end
-      #puts arr.to_s
+      # puts arr.to_s
       return arr
     end
 
@@ -41,23 +41,23 @@ module ActiveRecord
     def self.find_by(attribute)
       # Get all objects of class
       objs_arr = self.all
-      #puts objs_arr
+      # puts objs_arr
       found_objs = []
       # We get argument in Hash, So get key and value from that hash which is useful to find data from objevt
       # Because key is a column name of table and value is a value which we have to find from that column 
       attribute_name = attribute.keys
-      #puts attribute_name[0]
+      # puts attribute_name[0]
       attribute_value = attribute.values
-      #puts attribute_value[0]
+      # puts attribute_value[0]
 
       # find matching value from objects and return that object as output of this method 
       objs_arr.each do |obj|
         if obj.send(attribute_name[0]) == attribute_value[0].to_s
           found_objs.push obj
-          #puts find_obj
+          # puts find_obj
         end
       end
-      #puts found_objs.to_s
+      # puts found_objs.to_s
       if found_objs.length == 1
         return found_objs[0]
       else
@@ -69,7 +69,7 @@ module ActiveRecord
     def self.find(id_no)
       # Get all objects of class
       objs_arr = self.all
-      #puts objs_arr
+      # puts objs_arr
 
       objs_arr.each do |obj|
         if obj.send(:id) == id_no.to_s
@@ -82,9 +82,9 @@ module ActiveRecord
 
     # has_many
     def self.has_many(*args)
-      #puts "self = #{self}"
+      # puts "self = #{self}"
       class_name = self
-      #puts args.to_s
+      # puts args.to_s
       if args.length == 1
         associate_class_name = args[0].to_s.singularize.capitalize
         
@@ -92,45 +92,47 @@ module ActiveRecord
 
         # This line of code calls the send method with the message :define_method passing it method_name as the name of the method.
         send :define_method, method_name do
-          #puts self.id
-          hash = {}
-          hash[(class_name.to_s + "_id").downcase.to_sym] = self.id.to_i
-          #puts hash
-          # Convert string into class name and call find_by method on that class
-          objs = Object.const_get(associate_class_name).find_by hash
-          #puts objs.to_s
+          # puts self.id
+          argument_for_find_hash = {}
+          argument_for_find_hash[(class_name.to_s + "_id").downcase.to_sym] = self.id.to_i
+          # puts hash
+          # Convert string into class name and call find_by method on that class which is done by Object.const_get
+          # Get all objects as a result of first class_method call, on class
+          objs = Object.const_get(associate_class_name).find_by argument_for_find_hash
+          # puts objs.to_s
           return objs
         end
       else
         # Code for :through
         final_objects = []
         through_class_name = args[1].values[0]
-        #puts through_class_name
+        # puts through_class_name
         associate_class_name = args[0].to_s.singularize.capitalize
         
         method_name = (args[0].to_s).to_sym
 
         # This line of code calls the send method with the message :define_method passing it method_name as the name of the method.
         send :define_method, method_name do
-          #puts self.id
-          first_hash = {}
-          first_hash[(class_name.to_s + "_id").downcase.to_sym] = self.id.to_i
-          #puts first_hash
-          # Convert string into class name and call find_by method on that class
-          through_objs = Object.const_get(through_class_name.capitalize).find_by first_hash
-          #puts through_objs.to_s
-          # Find object in main associate table
+          # puts self.id
+          argument_for_find_hash = {}
+          argument_for_find_hash[(class_name.to_s + "_id").downcase.to_sym] = self.id.to_i
+          # puts first_hash
+          # Convert string into class name and call find_by method on that class which is done by Object.const_get
+          # Get all objects by calling find_by method for STEP-1
+          through_objs = Object.const_get(through_class_name.capitalize).find_by argument_for_find_hash
+          # puts through_objs.to_s   
           through_objs.each do |obj|
-            #puts obj.id
-            second_hash = {}
-            second_hash[(through_class_name.to_s + "_id").downcase.to_sym] = obj.id.to_i
-            #puts second_hash
-            pre_final_objects = Object.const_get(args[0].to_s.singularize.capitalize).find_by second_hash
+            # puts obj.id
+            argument_for_find_second_hash = {}
+            argument_for_find_second_hash[(through_class_name.to_s + "_id").downcase.to_sym] = obj.id.to_i
+            # puts second_hash
+            # Get all objects by calling find_by method for STEP-2
+            pre_final_objects = Object.const_get(args[0].to_s.singularize.capitalize).find_by argument_for_find_second_hash
             pre_final_objects.each do |obj_value|
               final_objects.push obj_value
             end
           end
-          #puts final_objects.to_s
+          # puts final_objects.to_s
           return final_objects
         end
       end
